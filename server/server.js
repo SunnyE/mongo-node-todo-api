abdc7730e14bb5e4c9174c6a940ec2cdf169617d
Ethan Favia <efavia215@live.com>
2017-04-26 15:39:24 -0400

    small fix

--- server/server.js
+++ server/server.js
@@ -3,3 +3,14 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise; 
 mongoose.connnect('mongodb://localhost:27017/TodoApp');
 
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
