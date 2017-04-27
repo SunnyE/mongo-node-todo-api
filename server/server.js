3235dad9788340ff01d9c4c9c644d014cd310c77
Ethan Favia <efavia215@live.com>
2017-04-27 16:42:11 -0400

    validation for the todo schema

--- server/server.js
+++ server/server.js
@@ -1,26 +1,43 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; 
-mongoose.connnect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'Get nintendo Gift card'
+// var newTodo = new Todo({
+//     text: 'Get nintendo Gift card'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved todo', doc)
+// }, (e) => {
+//     console.log('Unable to save todo')
+// });
+
+otherTodo = new Todo({
+    // text: 'eat lunch', 
+    // completed: true, 
+    // completedAt: 123
 });
 
-newTodo.save().then((doc) => {
+otherTodo.save().then((doc) => {
     console.log('Saved todo', doc)
 }, (e) => {
-    console.log('Unable to save todo')
+    console.log('unable to save todo')
 });
