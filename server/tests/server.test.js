bc70e486b14b4e2e79c155147bb3d8e45ba35e1f
Ethan Favia <efavia215@live.com>
2017-05-02 17:17:23 -0400

    more sever testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -21,7 +21,7 @@ describe('POST /todos', () => {
             })
             .end((err, res) => {
                 if (err) {
-                    return dpne(err)
+                    return done(err)
                 }
                 Todo.find().then((todos) => {
                     expect(todos.length).toBe(1);
@@ -29,5 +29,21 @@ describe('POST /todos', () => {
                     done();
             }).catch((e) => done(e));
         });
-    }); 
+    });
+    it('should not create todo with invalid body data', (done) => {
+
+        request(app)   
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+            })
+    });
 });
