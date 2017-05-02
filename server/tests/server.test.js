a4befa60f56fce4758b477b20fc0fd8e0ee06fd7
Ethan Favia <efavia215@live.com>
2017-05-02 16:17:08 -0400

    testing for the server

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,12 +4,30 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
+
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
         var text = 'Test todo post';
 
         request(app)
             .post('/todos')
-            .send 
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return dpne(err)
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+            }).catch((e) => done(e));
+        });
     }); 
 });
