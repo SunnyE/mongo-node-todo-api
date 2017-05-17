81e0ce9062ab264a989f849a659b170a3ae10cc2
Ethan Favia <efavia215@live.com>
2017-05-17 12:40:14 -0400

    work on the user post route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 require('./config/config');
 
-
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -99,6 +98,19 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User (body);
+
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(); 
+    })
+    
 });
 
 app.listen(port, () => {
