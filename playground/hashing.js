623ec522414a1a48290728b024bbc31ff784d52a
Ethan Favia <efavia215@live.com>
2017-05-18 11:49:17 -0400

    more updates

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,30 +1,42 @@
 const {SHA256} = require('crypto-js');
 
-var message = 'I am user number 2';
-
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-
-console.log(`Hash: ${hash}`);
+const jwt = require('jsonwebtoken');
 
 var data = {
-    id: 4
+    id:10
 };
 
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data)= 'somesecret').toString()
-}
+var token = jwt.sign(data, '123abc');
 
-token.data.id = 5;
+var decoded = jwt.verify(token, '123abc');
 
-token.hash = SHA256(JSON.stringify(token.data)).toString();
+console.log('decoded', decoded);
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// var message = 'I am user number 2';
 
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('data was changed');
-}
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+//     id: 4
+// };
+
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data)= 'somesecret').toString()
+// }
+
+// token.data.id = 5;
+
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+//     console.log('Data was not changed');
+// } else {
+//     console.log('data was changed');
+// }
